4cda1c89bf7f6f527e1ee6ea7284e5d8b1273c8d
Sake Radhika <pujithasakepujitha@gmail.com>
2025-06-10 13:15:23 +0530

    Binary Search.py

---
+++ Binary Search.py
@@ -0,0 +1,13 @@
+def binary_search(lst, target):
+    low, high = 0, len(lst) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if lst[mid] == target:
+            return mid
+        elif lst[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+print(binary_search([1, 2, 3, 4, 5], 4))
